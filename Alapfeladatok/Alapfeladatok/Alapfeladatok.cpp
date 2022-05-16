46c36db2b6b9c1729f66d3e6e08baf020149403a
Petint <petras.balint04@gmail.com>
2022-05-16 19:19:15 +0200

    Alapfeladatok szövegszám kész

--- Alapfeladatok/Alapfeladatok/Alapfeladatok.cpp
+++ Alapfeladatok/Alapfeladatok/Alapfeladatok.cpp
@@ -6,12 +6,21 @@
 
 using namespace std;
 
+bool isNumber(const string& str)
+{
+    for (char const& c : str) {
+        if (std::isdigit(c) == 0) return false;
+    }
+    return true;
+}
+
 void szovegszam()
 {
     string du;
     cout << "Egy beolvasott stringrõl állapítjuk meg, hogy az egész szám-e vagy sem.\nAdj meg egy számot: ";
     cin >> du;
-    cout << du;
+    // cout << du;
+    isNumber(du) ? cout << "Ez egy egyész szám,\n" : cout << "Ez nem egy egész szám";
 }
 
 int main()
