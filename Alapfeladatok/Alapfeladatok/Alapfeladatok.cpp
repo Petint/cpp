c4a6f183fc2a1212157afa723f15ed77f098eeda
Petint <petras.balint04@gmail.com>
2022-05-16 18:09:12 +0200

    Alapfeladatok/szovegszam/1

--- Alapfeladatok/Alapfeladatok/Alapfeladatok.cpp
+++ Alapfeladatok/Alapfeladatok/Alapfeladatok.cpp
@@ -2,21 +2,19 @@
 //
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main()
+void szovegszam()
 {
-    cout << "Hello World!\n";
+    string du;
+    cout << "Egy beolvasott stringrõl állapítjuk meg, hogy az egész szám-e vagy sem.\nAdj meg egy számot: ";
+    cin >> du;
+    cout << du;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+int main()
+{
+    szovegszam();
+}
