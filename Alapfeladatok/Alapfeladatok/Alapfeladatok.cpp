f76d9c1d6bb442444af29a42f3fd3c03448696b9
Petint <petras.balint04@gmail.com>
2022-05-21 16:23:31 +0200

    szöveghossz

--- Alapfeladatok/Alapfeladatok/Alapfeladatok.cpp
+++ Alapfeladatok/Alapfeladatok/Alapfeladatok.cpp
@@ -1,6 +1,3 @@
-// Alapfeladatok.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include <string>
 
@@ -9,17 +6,17 @@ using namespace std;
 bool isNumber(const string& str)
 {
     for (char const& c : str) {
-        if (std::isdigit(c) == 0) return false;
+        if (isdigit(c) == 0) return false;
     }
     return true;
 }
 
 void szovegszam()
 {
+    // Olvassunk be egy számot. Ha nem egész szám, akkor írjuk ki, hogy "Nem egész szám", különben írjuk ki, hogy "Egész szám".
     string du;
     cout << "Egy beolvasott stringrol allapítjuk meg, hogy az egesz szam-e vagy sem.\nAdj meg egy szamot: ";
     cin >> du;
-    // cout << du;
     isNumber(du) ? cout << "Ez egy egyesz szam,\n" : cout << "Ez nem egy egesz szam";
 }
 
@@ -50,8 +47,9 @@ void nagyobb()
     cout << "\n";
 }
 
-void paros()
+void Paros()
 {
+    // Olvassunk be egy egész számot. Ha nem egész szám, akkor írjuk ki, hogy "Nem egész szám", különben írjuk ki, hogy páros-e vagy páratlan.
     int num1;
     cout << "Egy beadott szamrol megalapitjuk, hogy paros-e vagy paratlan.\nAdd meg a szamot: ";
     cin >> num1;
@@ -65,10 +63,34 @@ void paros()
     }
 }
 
+void Szoveghossz()
+{
+    // Olvass be két szöveget és írd ki a rövidebbet. Ha egyforma hosszúak, akkor írd ki, hogy egyformák.
+    string du1;
+    string du2;
+    cout << "Két beolvasott szövegrõl állapítjuk meg, hogy melyik a rövidebb.\nAdj meg egy szöveget: ";
+    cin >> du1;
+    cout << "Adj meg egy másik szöveget: ";
+    cin >> du2;
+    if (du1.length() > du2.length())
+    {
+        cout << "Az elso szoveg hosszabb.";
+    }
+    else if (du1.length() < du2.length())
+    {
+        cout << "A masodik szoveg hosszabb.";
+    }
+    else
+    {
+        cout << "A ket szöveg hossza egyenlo.";
+    }
+}
+
 int main()
 {
     szovegszam();
     nagyobb();
-    paros();
+    Paros();
+    Szoveghossz();
     return 0;
 }
